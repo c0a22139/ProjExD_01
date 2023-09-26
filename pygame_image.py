e6adfd2a56f582615af6bba03785156f4a97a372
Yagi <c0a221392c@edu.teu.ac.jp>
2023-09-26 17:04:30 +0900

    背景

--- pygame_image.py
+++ pygame_image.py
@@ -4,9 +4,10 @@ import pygame as pg
 
 def main():
     pg.display.set_caption("はばたけ！こうかとん")
-    screen = pg.display.set_mode((1000, 600))
+    screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
+    bg_img_2 = pg.transform.flip(bg_img, True, False)
     c3_img = pg.image.load("ex01/fig/3.png")
     c3_img = pg.transform.flip(c3_img, True, False)
     #c3_img_2 = pg.transform.rotozoom(c3_img, 5, 1.0)
@@ -18,16 +19,20 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        x = tmr%1600
-        screen.blit(bg_img, [-x, 0])
-        screen.blit(bg_img,[1600-x,0])
+        x = tmr%6400
+        screen.blit(bg_img,[-x,0])
+        screen.blit(bg_img_2,[1600-x,0])
+        screen.blit(bg_img, [3200-x, 0])
+        screen.blit(bg_img_2,[4800-x,0])
+        screen.blit(bg_img,[6400-x, 0])
         screen.blit(list_c3[tmr%2],[300,200])
         pg.display.update()
-        tmr += 25
-   
+        tmr += 10
+       
+           
         
         
-        clock.tick(20)
+        clock.tick(100)
 
 
 if __name__ == "__main__":
